2ff774c0a7281d26c18ee2b417d4e670058d8bb5
habitoai <info@sash.co.ke>
2025-04-20 06:46:24 +0300

    i

---
+++ hosting-files/_next/static/chunks/pages/error-37670b865b07710a.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[510],{9385:function(n,u,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/error",function(){return r(9369)}])},9369:function(n,u,r){"use strict";r.r(u);var t=r(5893),_=r(5518);u.default=function(){return(0,t.jsx)(_.Z,{})}}},function(n){n.O(0,[261,518,774,888,179],(function(){return u=9385,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
+//# sourceMappingURL=error-37670b865b07710a.js.map
